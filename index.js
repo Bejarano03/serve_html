25992c58eab7916b454c342ba7028a0c79a55c97
Bejarano03 <69337575+Bejarano03@users.noreply.github.com>
2024-01-24 16:46:49 -0800

    finished creating a simple server

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+// import necessary modules
 import http from 'http';
 import fs from 'fs';
 
+// creates server
 http.createServer((req,res) => {
-    res.writeHead(200, {'Content-Type': 'text/htm'});
-    res.write('Hello World!');
-    res.end();
+    // handles the different URLs
+    if(req.url === '/about') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('About Page');
+        res.end();
+    } else if (req.url === '/') {
+        fs.readFile('home.html', (err, data) => {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            res.end();
+        })
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.write('404 Page Not Found');
+        res.end();
+    }
 }).listen(3000);
